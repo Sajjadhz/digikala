494f6c2529fc8a0d6fe9831b02aa335d50ad3193
MSalehJ <M.jalalian1999@gmail.com>
2021-02-14 20:55:04 +0330

    adding  basket1

--- digikala/settings.py
+++ digikala/settings.py
@@ -18,8 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_^g94726s@hgr!pr1&ymv3eyiosd&0g!m(8@_6s)zbtw#qo^h3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
